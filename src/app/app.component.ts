3ddbda42097aa95eff84c3b119981ae760d5126b
Abderrezak <abderrezak.boukazouha@etudiant.univ-rennes1.fr>
2022-02-20 13:06:47 +0100

    tp done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,17 @@ import { Pokemondata, OfficialArtwork } from './pokemondata';
 import { Component } from '@angular/core';
 import axios from 'axios';
 
-
-
-
-
 interface Pokemon {
-  name: string,
-  url: string
+  name: string;
+  url: string;
 }
 @Component({
   selector: 'app-root',
-  templateUrl: "./app.component.html",
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-
-
 export class AppComponent {
-
- 
-  pokemondata : Pokemondata = {
+  pokemondata: Pokemondata = {
     abilities: [],
     base_experience: 0,
     forms: [],
@@ -38,60 +30,60 @@ export class AppComponent {
     types: [],
     weight: 0,
     species: {
-      name : '',
-      url : ''
+      name: '',
+      url: '',
     },
     sprites: {
-      back_default:       "",
-      back_female:        null,
-      back_shiny:         "",
-      back_shiny_female:  null,
-      front_default:      "",
-      front_female:       null,
-      front_shiny:        "",
+      back_default: '',
+      back_female: null,
+      back_shiny: '',
+      back_shiny_female: null,
+      front_default: '',
+      front_female: null,
+      front_shiny: '',
       front_shiny_female: null,
-    }
-  }
+    },
+  };
   pokemons = new Array<Pokemon>();
-  show = false
-  name = ""
-  baseExperience = 0
-  number = 0
- 
-  pic = ""
-  moves :Array<string> = [] 
-  types :Array<string> = [] 
-  ngOnInit() {this.getAllPokemons()};
+  show = false;
+  name = '';
+  baseExperience = 0;
+  number = 0;
 
- 
-   async getAllPokemons () {
-  const request = await axios.get("https://pokeapi.co/api/v2/pokemon")
-   this.pokemons = request.data.results
-  return this.pokemons
+  pic = '';
+  moves: Array<string> = [];
+  types: Array<string> = [];
+  ngOnInit() {
+    this.getAllPokemons();
   }
 
-  async showDetails (num : number) {
-    const details = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
-    this.moves = []
-    console.log(this.pokemondata)
-    this.show = true
-    this.name = details.data.name ;
-    this.baseExperience = details.data.base_experience
-    this.number= details.data.order
-    this.pic = details.data.sprites.other.dream_world.front_default
-    details.data.moves.map((m: { move: { name: any; }; })=> {
-      if(this.moves.length == 5) {return}
-      return this.moves.push(m.move.name) 
-    })
-
-    this.types = []
-    details.data.types.map( (type: { type: { name: string; }; }) => {
-      return this.types.push(type.type.name)
-    })
-
-
-
+  async getAllPokemons() {
+    const request = await axios.get(
+      'https://pokeapi.co/api/v2/pokemon/?limit=150'
+    );
+    this.pokemons = request.data.results;
+    return this.pokemons;
   }
-  
 
+  async showDetails(num: number) {
+    const details = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+    this.moves = [];
+    console.log(this.pokemondata);
+    this.show = true;
+    this.name = details.data.name;
+    this.baseExperience = details.data.base_experience;
+    this.number = details.data.order;
+    this.pic = details.data.sprites.other.dream_world.front_default;
+    details.data.moves.map((m: { move: { name: any } }) => {
+      if (this.moves.length == 5) {
+        return;
+      }
+      return this.moves.push(m.move.name);
+    });
+
+    this.types = [];
+    details.data.types.map((type: { type: { name: string } }) => {
+      return this.types.push(type.type.name);
+    });
+  }
 }
